83ecb2edf1d173550d4f3bdf42bc5f695eeb5f48
hatem ben tayeb <hatemtayeb2@gmail.com>
2018-10-23 00:22:26 +0100

    groupbox 1 : input data groupbox 2 : view data
    ==> create main application GUI
    progress bar
    
    getting data from computer (cpu and disk usage for our script)
    all done
    just plotting data is missing
    i will fixe it soon

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 import sys
 import time
-import numpy as np
+import subprocess as sub
+import os
 import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QGroupBox, QFormLayout, QPushButton, QLabel, QLineEdit, \
     QVBoxLayout, QTableWidget,QProgressBar,QTableWidgetItem
@@ -21,35 +22,41 @@ class app_eval(QWidget):
         self.nbrF = QLineEdit("1")
         self.tht = QLabel("Thinking time")
         self.thtF = QLineEdit("1")
-        self.T = QLabel("durée d\'observation")
+        self.T = QLabel("Nurée d\'observation")
         self.TF = QLineEdit("1")
+        self.blk = QLabel("Nombre de bloque")
+        self.blkF = QLineEdit("1")
         self.start = QPushButton("Start")
+        self.clear = QPushButton("clear")
         self.progbar = QProgressBar()
 
         #-------------------------------------------------
         self.qb1 = QGroupBox("View DATA")
         self.qV = QVBoxLayout()
         self.table = QTableWidget()
-        self.table.setRowCount(6)
+        self.table.setRowCount(50)
         self.table.setColumnCount(6)
         self.table.setHorizontalHeaderLabels(["BTcpu1", "BTdisk1", "BTcpu2", "BTdisk2", "BTcpu3", "BTdisk3"])
 
-        self.table.resizeColumnsToContents()
+        #self.table.resizeColumnsToContents()
 
         self.AsymTR = QPushButton("Asymptote Temps de Reponse")
         self.AsymDS = QPushButton("Asymptote Debit du systeme")
-
+        self.load   = QPushButton("Load Data")
 
 
         #-------------------------------------------------
         self.qb1.setLayout(self.qV)
         self.qV.addWidget(self.table)
+        self.qV.addWidget(self.load)
         self.qV.addWidget(self.AsymTR)
         self.qV.addWidget(self.AsymDS)
+
         self.qf.addRow(self.nbr, self.nbrF)
         self.qf.addRow(self.tht, self.thtF)
         self.qf.addRow(self.T, self.TF)
-        self.qf.addRow(self.start)
+        self.qf.addRow(self.blk, self.blkF)
+        self.qf.addRow(self.start,self.clear)
         self.qf.addRow(self.progbar)
         self.qb.setLayout(self.qf)
         self.grid.addWidget(self.qb,0,0)
@@ -59,10 +66,12 @@ class app_eval(QWidget):
         self.x = int(self.TF.text())
         self.start.clicked.connect(lambda: progress(self, int(self.TF.text())))
         self.AsymTR.clicked.connect(lambda :plot_data())
+        self.load.clicked.connect(lambda :setDaTa(self))
+        self.clear.clicked.connect(lambda :clearData())
 
 
-
-
+def clearData():
+    os.system('rm *.{csv,txt}')
 
 
 def progress(self,n):
@@ -70,16 +79,31 @@ def progress(self,n):
         time.sleep(1)
         x=(i/n)*100
         self.progbar.setValue(x)
+        os.system(os.path.join("test","disk_cpu.sh")+" "+self.blkF.text()+" "+ self.nbrF.text())
 
-    setDaTa(self)
 
 def setDaTa(self):
-    np.set_printoptions(precision=2)
-    for i in range(0,6):
-        for j in range(0,6):
-              x= np.random.rand()
+    cp=0
+    with open('finaldisk.csv','r') as f:
+        xx= f.read().split(',')
+        for i in xx:
 
-              self.table.setItem(i, 0, QTableWidgetItem(str(x)))
+              if i != None:
+                  self.table.setItem(cp, 1, QTableWidgetItem(str(i)))
+                  cp += 1
+              else:
+                  pass
+    cp1 = 0
+    with open('finalcpu.csv', 'r') as f:
+            xx = f.read().split(',')
+            for i in xx:
+
+                if i != None:
+                    self.table.setItem(cp1, 0, QTableWidgetItem(str(i)))
+
+                    cp1 += 1
+                else:
+                    pass
 
 def plot_data():
     plt.xlim(50)
